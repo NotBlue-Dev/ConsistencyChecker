d39637c4a72b436d65060125fdbc89415f209538
NotBlue <enzodubocage@gmail.com>
2022-11-22 21:31:25 +0100

    inité

---
+++ viewer/renderer.js
@@ -0,0 +1,15 @@
+let scene = new THREE.Scene();
+let aspectRatio = window.innerWidth / window.innerHeight;
+let camera = new THREE.PerspectiveCamera(75, aspectRatio, 1, 10000);
+camera.position.z = 450;
+scene.add(camera);
+
+let bigSphere = new THREE.SphereGeometry(100, 20, 20);
+let cover = new THREE.MeshNormalMaterial();
+let nucleus = new THREE.Mesh(bigSphere, cover);
+scene.add(nucleus);
+
+let renderer = new THREE.CanvasRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+renderer.render(scene, camera);
