e9ceb7e55e55f2c59daccfa8e2e8f1fce69e12f9
NotBlue <enzodubocage@gmail.com>
2022-11-24 23:47:09 +0100

    disc in goal detection (broken)

--- src/echo-arena-events/goal.js
+++ src/echo-arena-events/goal.js
@@ -1,5 +1,5 @@
+/* eslint-disable no-invalid-this */
 /*
- * to do:  le disque traverse le goal et le joueur qui a touché le disque est le client
  * pocket gauche : -1.5, 0.0, -36
  * pocket droit : 1.5, 0.0, -36
  * pocket haut : 0.0, 1.5, -36
@@ -7,20 +7,49 @@
  */
 class goal {
     constructor() {
-        this.lastScore = null;
+        this.inGoal = false;
+        this.blueGoal = [[-1.5, 0.0, -36], [1.5, 0.0, -36], [0.0, 1.5, -36], [0.0, -1.5, -36]];
+        this.orangeGoal = [[-1.5, 0.0, 36], [1.5, 0.0, 36], [0.0, 1.5, 36], [0.0, -1.5, 36]];
     }
 
     handle(gameData, eventEmitter) {
-        if(JSON.stringify(this.lastScore) === JSON.stringify(gameData.lastScore)) {
-            return;
+        function insideGoal(disc, goal, inside) {
+            // https://math.stackexchange.com/questions/312403/how-do-i-determine-if-a-point-is-within-a-rhombus
+            
+            //CA MARCHE PAS DUTOUT
+            let A = goal[0];
+            let B = goal[1];
+            let C = goal[2];
+            let D = goal[3];
+            let Q = [0.5 * (A[0] + C[1]),0.5 * (A[0] + C[1])];
+            // a = 0.5 * distance between A and C
+            let a = 0.5 * (Math.sqrt(Math.pow(C[0] - A[0], 2) + Math.pow(C[1] - A[1], 2)));
+            // b=1/2*∥D−B∥
+            let b = 0.5 * (Math.sqrt(Math.pow(D[0] - B[0], 2) + Math.pow(D[1] - B[1], 2)));
+            // unit vector in x-direction
+            let U = [(C[0] - A[0]) / (2 * a), (C[1] - A[1]) / (2 * a)];
+            // unit vector in y-direction
+            let V = [(D[0] - B[0]) / (2 * b), (D[1] - B[1]) / (2 * b)];
+            let W = [disc[0] - Q[0], disc[1] - Q[1]];
+            //here W*U is the dot product of W and U
+            let xabs = Math.abs((W[0] * U[0]) + (W[1] * U[1]));
+            let yabs = Math.abs((W[0] * V[0]) + (W[1] * V[1]));
+            if ((xabs / a) + (yabs / b) <= 1) {
+                if(inside === false) {
+                    return true;
+                }
+            }
         }
 
-        if(this.lastScore !== null) {
-            console.log("score");
-            eventEmitter.send('three.render', "score");
+        let blue = insideGoal([1.2,1], this.blueGoal, this.inGoal);
+        let orange = insideGoal([1.2,1], this.orangeGoal, this.inGoal);
+        if(blue || orange) {
+            this.inGoal = true;
+            eventEmitter.send('three.render', "goal");
+            console.log("goal");
+        } else {
+            this.inGoal = false;
         }
-
-        this.lastScore = gameData.lastScore;
     }
 }
 
